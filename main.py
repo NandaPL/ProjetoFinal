3a35d5bdf8acc4e80ac2d57e2b443205d107276f
Fernanda Pinto Lopes <fpl.snf19@uea.edu.br>
2019-11-15 18:26:43 -0400

    Temos a tela

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pygame
 
 pygame.init()
+screen = pygame.display.set_mode((600,350))
+pygame.display.set_caption('Projeto Final')
+pygame.display.flip()
+
+run = init = True
+
+while run:
+    if init:
+        screen
+
+    pygame.display.update()
