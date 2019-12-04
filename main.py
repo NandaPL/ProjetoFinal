1239bc1338da407e9b3ce309b5f114272d369f7f
vanessadcamara <vanessadcamara@gmail.com>
2019-12-04 12:17:00 -0400

    Adicionando backgound sounds

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from show_credits import run_credits
 from all_sprites import *
 import sprites
 import pygame
+import sounds
+
+pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 
 width = 800
@@ -43,12 +46,17 @@ def menu():
     screen.blit(back_menu, (0, 0))
     play_press = screen.blit(button_play, (300, 200))
     credits_press = screen.blit(button_credits, (285, 350))
-    screen.blit(name_game, (200,50))
+    screen.blit(name_game, (200, 50))
 
 
 run = True
 initial = True
 button_pressed = False
+# background sound
+
+pygame.mixer.music.load("sounds/Dream_Island.mp3")
+pygame.mixer.music.set_volume(1)
+pygame.mixer.music.play(-1)
 
 while run:
 
@@ -63,7 +71,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    if button_pressed: # aqui eu verifico se o play foi
+    if button_pressed:  # aqui eu verifico se o play foi
         mouse = pygame.mouse.get_pos()
         if mouse[0] > 314 and mouse[0] < 481:
             if mouse[1] > 214 and mouse[1] < 274:
@@ -77,15 +85,19 @@ while run:
         frame = 8
     if frame >= 8:
         if frame % 8 == 0:
-            knight = load_image(sprite_idle[int (frame/8)])
+            knight = load_image(sprite_idle[int(frame/8)])
     frame += 1
-    
+
     redraw_knight(50, 300)
     redraw_knight(650, 300)
 
     if pygame.mouse. get_focused():
         mouse = pygame.mouse.get_pos()
         screen.blit(cursor, mouse)
+    # background sound
+    # pygame.mixer.music.load("sounds/Dream_Island.mp3")
+    # pygame.mixer.music.set_volume(1)
+    # pygame.mixer.music.play(-1)
 
     pygame.display.flip()
     clock.tick(60)
